c9ebf387da5840b7852c473ab46f6cf581363c02
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-30 19:51:27 -0500

    UI Design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./component/common/SideBar";
+import RightPanel from "./component/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+function App() {
+
+
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage/>} />
+        <Route path='/profile/:username' element={<ProfilePage/>} />
+      </Routes>
+      <RightPanel />
+    </div>
+  );
+}
+
+export default App;
